e9714baca51703c21250f6f595d02be803f26478
OleksandrOrz <111054001+OleksandrOrz@users.noreply.github.com>
2022-08-17 13:38:44 +0300

    Added JS search engine and current location

--- src/app.js
+++ src/app.js
@@ -19,5 +19,42 @@ if (hour < 10) {
 if (min < 10) {
   min = "0" + hour;
 }
-let time = document.querySelector("#time");
-time.innerHTML = `${days[now.getDay()]} ${hour}:${min}`;
+document.querySelector("#time").innerHTML = `${
+  days[now.getDay()]
+} ${hour}:${min}. `;
+
+// City search
+function currentWeatherCity(res) {
+  console.log();
+  document.querySelector("h1").innerHTML = `${res.data.name}`;
+  document.querySelector("#humidity").innerHTML = `${res.data.main.humidity}%`;
+  document.querySelector("#wind").innerHTML = `${Math.round(
+    res.data.wind.speed
+  )} km/h`;
+  document.querySelector("#temp").innerHTML = Math.round(res.data.main.temp);
+  document.querySelector("#description").innerHTML =
+    res.data.weather[0].description;
+}
+function citySearch(event) {
+  event.preventDefault();
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${
+    document.querySelector("input").value
+  }&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(currentWeatherCity);
+}
+document.querySelector("form").addEventListener("submit", citySearch);
+
+// City by default
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(currentWeatherCity);
+
+// Weather in current location
+function findLoc(res) {
+  apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${res.coords.latitude}&lon=${res.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(currentWeatherCity);
+}
+function curLoc() {
+  let navig = navigator.geolocation.getCurrentPosition(findLoc);
+  console.log(navig);
+}
+document.querySelector("#currentLocation").addEventListener("click", curLoc);
